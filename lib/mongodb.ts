48a27ac1942520da63088ca2ad14550d845530ac
Adit09014 <dpsvn.adit09014@gmail.com>
2026-08-05 00:04:15 +0530

    Fixed Deployment Issue

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,6 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
@@ -11,14 +10,18 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (process.env.NODE_ENV === "development") {
+if (!uri) {
+  clientPromise = Promise.reject(
+    new Error("Please add your Mongo URI to Vercel Environment Variables as MONGODB_URI.")
+  );
+} else if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    const client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  const client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
